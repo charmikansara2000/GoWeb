16713f08b692208779886a85ca227579c91601f0
charmikansara2000 <charmi.kansara@bacancy.com>
2021-02-10 17:18:16 +0530

    added

---
+++ main.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var db *gorm.DB
+
+type User struct {
+	gorm.Model
+
+	Name  string `json:"name"`
+	Email string `json:"email"`
+	Phone string `json:"phone"`
+}
+
+func migration() {
+	var err error
+	db, err = gorm.Open(postgres.Open("postgres://postgres:postgres@localhost/UserDB?sslmode=disable"), &gorm.Config{})
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		fmt.Println(err)
+	}
+	db.AutoMigrate(&User{})
+
+}
+func getAllUsers(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var users []User
+	db.Find(&users)
+	json.NewEncoder(w).Encode(users)
+}
+func createUser(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var user User
+	json.NewDecoder(r.Body).Decode(&user)
+	db.Create(&user)
+	json.NewEncoder(w).Encode(user)
+
+}
+func getOneUser(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var user User
+	params := mux.Vars(r)
+	db.First(&user, params["id"])
+	json.NewEncoder(w).Encode(user)
+}
+
+func updateUser(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var user User
+	params := mux.Vars(r)
+	db.First(&user, params["id"])
+	json.NewDecoder(r.Body).Decode(&user)
+	db.Save(&user)
+	json.NewEncoder(w).Encode(user)
+}
+func deleteUser(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var user User
+	params := mux.Vars(r)
+	db.Delete(&user, params["id"])
+	//json.NewEncoder(w).Encode("deleted")
+}
+func routes() {
+	r := mux.NewRouter()
+	r.HandleFunc("/user", getAllUsers).Methods("GET")
+	r.HandleFunc("/user", createUser).Methods("POST")
+	r.HandleFunc("/user/{id}", getOneUser).Methods("GET")
+	r.HandleFunc("/user/{id}", updateUser).Methods("PUT")
+	r.HandleFunc("/user/{id}", deleteUser).Methods("DELETE")
+	log.Fatal(http.ListenAndServe(":8083", r))
+}
+func main() {
+	migration()
+	routes()
+}
