6dca275331e38408668e6f86a680baf0d160fcf4
charmikansara2000 <charmi.kansara@bacancy.com>
2021-02-24 15:01:28 +0530

    modified

--- main.go
+++ main.go
@@ -4,14 +4,28 @@ import (
 	"context"
 	"encoding/csv"
 	"encoding/json"
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+const (
+	conString      = "mongodb://localhost:27017"
+	dbName         = "test"
+	collectionName = "railway1"
+	limit          = 10
+)
+
+//var wg sync.WaitGroup
+var (
+	ch = make(chan int, limit)
 )
 
 type Data struct {
@@ -47,8 +61,8 @@ func ReadCsv(filename string) ([][]string, error) {
 	return lines, nil
 }
 
-func dbConn() *mongo.Collection {
-	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+func dbConn() (*mongo.Collection, *mongo.Client) {
+	clientOptions := options.Client().ApplyURI(conString)
 
 	// Connect to MongoDB
 	client, err := mongo.Connect(context.TODO(), clientOptions)
@@ -64,15 +78,18 @@ func dbConn() *mongo.Collection {
 		log.Fatal(err)
 	}
 
-	collection := client.Database("test").Collection("railway1")
+	collection := client.Database(dbName).Collection(collectionName)
 	fmt.Println("Connected to MongoDB!")
 
-	return collection
+	return collection, client
 
 }
 func getallTrains(w http.ResponseWriter, r *http.Request) {
 
-	collection := dbConn()
+	collection, client := dbConn()
+
+	defer client.Disconnect(context.TODO())
+
 	cursor, err := collection.Find(context.TODO(), bson.D{{}})
 
 	if err != nil {
@@ -89,8 +106,12 @@ func getallTrains(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.Write(bytedata)
 }
-func main() {
-	collection := dbConn()
+
+func insertData() {
+
+	collection, client := dbConn()
+
+	defer client.Disconnect(context.TODO())
 
 	csvData, err := ReadCsv("Indian_railway1.csv")
 	if err != nil {
@@ -100,28 +121,52 @@ func main() {
 	csvData = csvData[1:501]
 
 	for _, line := range csvData {
-		data := Data{
-			TrainNo:   line[0],
-			TrainName: line[1],
-			SEQ:       line[2],
-			Code:      line[3],
-			StName:    line[4],
-			ATime:     line[5],
-			DTime:     line[6],
-			Distance:  line[7],
-			SS:        line[8],
-			SSname:    line[9],
-			Ds:        line[10],
-			DsName:    line[11],
-		}
+		ch <- 1
+		//wg.Add(1)
+		func() {
+			//defer wg.Done()
+			data := Data{
+				TrainNo:   line[0],
+				TrainName: line[1],
+				SEQ:       line[2],
+				Code:      line[3],
+				StName:    line[4],
+				ATime:     line[5],
+				DTime:     line[6],
+				Distance:  line[7],
+				SS:        line[8],
+				SSname:    line[9],
+				Ds:        line[10],
+				DsName:    line[11],
+			}
 
-		_, err := collection.InsertOne(context.TODO(), data)
-		if err != nil {
-			panic(err)
-		}
-		//break
+			_, err := collection.InsertOne(context.TODO(), data)
+			if err != nil {
+				panic(err)
+			}
+			<-ch
+			//break
+		}()
+		//wg.Wait()
 
+		//mt.Println("done")
 	}
+	for i := 0; i < limit; i++ {
+		ch <- 1
+	}
+}
+func main() {
+	start := time.Now()
+	read := flag.Bool("insert", false, "a bool")
+	flag.Parse()
+	if *read {
+		insertData()
+	} else {
+		fmt.Println("failed")
+	}
+
+	elp := time.Since(start)
+	fmt.Println(elp)
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Welcome to my website!")
 	})
